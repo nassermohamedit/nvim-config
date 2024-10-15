e51d7b8a01d0be7061aa8443048958ae60b8222b
Nasser Mohamed <nassermohamedit@gmail.com>
2024-10-15 01:42:46 +0100

    switch to mason jdtls

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -5,18 +5,7 @@ local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 
 local config = {
     cmd = {
-      'java',
-      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-      '-Dosgi.bundles.defaultStartLevel=4',
-      '-Declipse.product=org.eclipse.jdt.ls.core.product',
-      '-Dlog.level=ALL',
-      '-Xmx1G',
-      '--add-modules=ALL-SYSTEM',
-      '--add-opens=java.base/java.util=ALL-UNNAMED',
-      '--add-opens=java.base/java.lang=ALL-UNNAMED',
-      '-jar', '/home/nasser/jdt/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-      '-configuration', '/home/nasser/jdt/config_linux/',
-      '-data', '/home/nasser/.cache/jdtls-workspace' .. workspace_dir,
+      vim.fn.expand('~/.local/share/nvim/mason/bin/jdtls')
     },
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
     capabilities = capabilities
