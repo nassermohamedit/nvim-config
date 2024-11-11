36f19a19ba741d90acd951f6b8c1955f74d9456d
Nasser Mohamed <nassermohamedit@gmail.com>
2024-11-11 15:41:11 +0100

    Disable syntax highlighting by default

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -215,3 +215,13 @@ vim.api.nvim_set_keymap(
 	"<Cmd>lua require'camellia'.run_main()<CR>",
 	{ noremap = true, silent = true }
 )
+
+
+vim.cmd("syntax off")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+});
